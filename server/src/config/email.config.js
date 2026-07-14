fddb769ee84169bb0ef9bff475a9105550963e21
Raj Vardhan <errvmaurya563@gmail.com>
2026-07-14 21:03:11 +0530

    feat: Enhance RestaurantPhotos component with image upload functionality and validation

--- server/src/config/email.config.js
+++ server/src/config/email.config.js
@@ -40,7 +40,7 @@ const sendEmail = async (to, subject, message) => {
 export default sendEmail;
 
 // sendEmail(
-//   "rajvardhan@ricr.in",
+//   "receiversemail@gmail.com",
 //   "test Email",
 //   `<h1 style='color:blue;'>Test Message</h1>
 //   <p style='color:red;'>Batch 6</p>`,
